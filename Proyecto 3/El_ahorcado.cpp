837cdd63808b7b82538206c2f87219c5c8b7501a
DaniRodez <daniel,plascencia6224@alumnos.udg.mx>
2026-04-16 18:27:24 -0600

    almost

--- Proyecto 3/El_ahorcado.cpp
+++ Proyecto 3/El_ahorcado.cpp
@@ -11,21 +11,29 @@ Programa: El ahorcado
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
+
+// VARIABLES DEL JUEGO
+    string letrasAdivinadas = "";
+    int intentosRestantes = 6;
+    bool gano = false;
 
 // PROTOTIPOS
 void jugarAhorcado();
 string seleccionarPalabra();
 void mostrarEstadoJuego(string palabra, string letrasAdivinadas, int intentosRestantes);
 void mostrarAhorcado(int intentosRestantes);
+void limpiarPantalla();
 
 int main() {
+    
     char game;
     do {
         jugarAhorcado();
 
-        cout << "¿Quieres jugar otra ronda? (s/n): ";
+        cout << "Quieres jugar otra ronda? (s/n): ";
         cin >> game;
     } while (game == 's' || game == 'S');
 
@@ -37,14 +45,14 @@ void jugarAhorcado() {
     string word;
 
     cout << "----------------------------------" << endl;
-    cout << "¡Bienvenido al juego del ahorcado!" << endl;
+    cout << "Bienvenido al juego del ahorcado!" << endl;
     cout << "----------------------------------" << endl;
 
     // elegir modo
     do {
-        cout << "1. Contra la máquina" << endl;
+        cout << "1. Contra la maquina" << endl;
         cout << "2. Contra un amigo" << endl;
-        cout << "Ingresa el número del modo: ";
+        cout << "Ingresa el numero del modo: ";
         cin >> mode;
     } while (mode != 1 && mode != 2);
 
@@ -57,18 +65,17 @@ void jugarAhorcado() {
         cin >> word;
     }
 
-    // VARIABLES DEL JUEGO
-    string letrasAdivinadas = "";
-    int intentosRestantes = 6;
-    bool gano = false;
-
+    
     // CICLO PRINCIPAL
     while (intentosRestantes > 0 && !gano) {
 
+        limpiarPantalla(); // limpiar pantalla para mostrar el progreso actualizado
+        
         mostrarAhorcado(intentosRestantes);
         mostrarEstadoJuego(word, letrasAdivinadas, intentosRestantes);
 
         char letra;
+        
         cout << "Ingresa una letra: ";
         cin >> letra;
 
@@ -100,10 +107,15 @@ void jugarAhorcado() {
 
     // RESULTADO FINAL
     if (gano) {
-        cout << "\n¡Ganaste! La palabra era: " << word << endl;
+        system("color 2F"); // cambiar color de fondo a verde
+        cout << "\nGanaste! La palabra era: " << word << endl;
+        Beep(1200, 200); // sonido agudo de victoria
+        gano = false; // resetear para la siguiente ronda
+
     } else {
-        mostrarAhorcado(0);
+        system("color 4F"); // cambiar color de fondo a rojo
         cout << "\nPerdiste. La palabra era: " << word << endl;
+        Beep(400, 300); // sonido grave de derrota
     }
 }
 
@@ -135,26 +147,60 @@ void mostrarEstadoJuego(string palabra, string letrasAdivinadas, int intentosRes
 void mostrarAhorcado(int intentosRestantes) {
 
     if (intentosRestantes == 6) {
-        cout << "  +---+\n  |   |\n      |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "      |\n";
+        cout << "      |\n";
+        cout << "      |\n";
     }
     else if (intentosRestantes == 5) {
-        cout << "  +---+\n  |   |\n  O   |\n      |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << "      |\n";
+        cout << "      |\n";
     }
     else if (intentosRestantes == 4) {
-        cout << "  +---+\n  |   |\n  O   |\n  |   |\n      |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << "  |   |\n";
+        cout << "      |\n";
     }
     else if (intentosRestantes == 3) {
-        cout << "  +---+\n  |   |\n  O   |\n /|   |\n      |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << " /|   |\n";
+        cout << "      |\n";
     }
     else if (intentosRestantes == 2) {
-        cout << "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << " /|\\  |\n";
+        cout << "      |\n";
     }
     else if (intentosRestantes == 1) {
-        cout << "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << " /|\\  |\n";
+        cout << " /    |\n";
     }
     else {
+        system("color 4F"); // cambiar color de fondo a rojo
         cout << "\033[41m";
-        cout << "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n";
-        cout << "Game Over!\n\033[0m";
+        cout << "  +---+\n";
+        cout << "  |   |\n";
+        cout << "  O   |\n";
+        cout << " /|\\  |\n";
+        cout << " / \\  |\n";
+        cout << "Game Over!";
     }
+}
+
+//funcion para limpiar pantalla
+void limpiarPantalla() {
+    system("cls");
 }
